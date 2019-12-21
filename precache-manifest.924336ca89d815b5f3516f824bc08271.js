930b6e3a1cea689b18d9f70862ebb9368fbefe56
Nate Harris <nate.harris@curology.com>
2019-12-21 12:40:27 -0800

    Updates

---
+++ precache-manifest.924336ca89d815b5f3516f824bc08271.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e352cd7469ba237c0c44574e0179bf4a",
+    "url": "/paint/index.html"
+  },
+  {
+    "revision": "18bc0163f020343d9e65",
+    "url": "/paint/static/js/2.75a33d25.chunk.js"
+  },
+  {
+    "revision": "0366945e5ad4ae220fd5",
+    "url": "/paint/static/js/main.78171f3a.chunk.js"
+  },
+  {
+    "revision": "7096fc9dd6dc265bcf56",
+    "url": "/paint/static/js/runtime~main.c237d022.js"
+  }
+]);
